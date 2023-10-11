6d417b6ed907fdff02092eb9dddef4d9b957c56a
Camila <camila.verssao@gmail.com>
2023-10-11 11:52:31 -0300

    [add] Cart Page

--- script.js
+++ script.js
@@ -1,24 +1,79 @@
+const products = [
+    {
+        id: 1,
+        title: "Dunk Jumbo Mushroom",
+        price: 899.99,
+        img: "../assets/products/prod1.png"
+    },
+    {
+        id: 2,
+        title: "Terminator Low Velvet Brown",
+        price: 1099.99,
+        img: "../assets/products/prod2.png"
+    },
+    {
+        id: 3,
+        title: "Adi 2000",
+        price: 799.99,
+        img: "../assets/products/prod3.png"
+    },
+    {
+        id: 4,
+        title: "Air Force 1 Low Retro",
+        price: 1399.99,
+        img: "../assets/products/prod4.png"
+    },
+    {
+        id: 5,
+        title: "Ozweego",
+        price: 999.99,
+        img: "../assets/products/prod5.png"
+    },
+    {
+        id: 6,
+        title: "Air Vapormax Triple Black",
+        price: 999.99,
+        img: "../assets/products/prod6.avif"
+    },
+    {
+        id: 7,
+        title: "Forum Mod Low",
+        price: 899.99,
+        img: "../assets/products/prod7.png"
+    },
+    {
+        id: 8,
+        title: "Air Jordan 1 Low GS Rabbit",
+        price: 999.99,
+        img: "../assets/products/prod8.avif"
+    }  
+];
 
-let image_sproduct = document.getElementById('MainImg');
+let choosenProduct = products[0];
+let productClass = Array.from(document.getElementsByClassName('product'));
 
-function info(index) {
+let sproductTitle = document.getElementById('prod-names');
+console.log(sproductTitle)
 
-    let product = document.getElementsByClassName('product')[index];
+const currentProductImg = document.querySelector('.img-object');
+const currentProductTitle = document.querySelector('.title-object');
+const currentProductPrice = document.querySelector('.price-object');
 
-    let sources = [
-        'assets/products/prod1.png',
-        'assets/products/prod2.png',
-        'assets/products/prod3.png',
-        'assets/products/prod4.png', 
-        'assets/products/prod5.png',
-        'assets/products/prod6.avif',
-        'assets/products/prod7.png',
-        'assets/products/prod1.avif'
-    ];
+productClass.forEach((item, index) => {
+    item.addEventListener('click', () => {
 
-    window.location.href = '/pages-html/sproduct.html';
+        choosenProduct = products[index];
+        console.log(choosenProduct)
 
-    return sources[index];
-}
+        //window.location.href = '/pages-html/sproduct.html';
+
+        //sproductTitle.textContent = choosenProduct.title;
+
+    });
+});
+
+
+
+
 
 
